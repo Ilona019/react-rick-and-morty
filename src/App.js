4776397ced5a1edb9bd8fab2a4c93fa2fb469273
Могилевский Иван Владимирович <mogilevsky@MacBook-Pro-Mogilevskij.local>
2019-11-27 20:17:07 +0300

    Add state and props
    Signed-off-by: Могилевский Иван Владимирович <mogilevsky@MacBook-Pro-Mogilevskij.local>

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import Person from "./components/Person";
 import Score from "./components/Score";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+      persons: [],
+      curIndex: 0
+    };
+  }
+
+  changeScore(status) {
+    this.setState({
+      score: this.state.score + (status === 'Alive' ? 1 : -1)
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <Score />
+        <Score count={this.state.score}/>
         <Person />
         <div className="buttonsRow">
-          <BlackButton caption="Alive" />
+          <BlackButton caption="Alive" onClick={() => this.changeScore('Alive')}/>
           <div className="subTitle">OR</div>
-          <BlackButton caption="Not" />
+          <BlackButton caption="Not" onClick={() => this.changeScore('Not')}/>
         </div>
       </div>
     );
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -3,6 +3,6 @@ import "./Score.css";
 
 export default class Score extends React.Component {
   render() {
-    return <div className="score-main">Score: 0</div>;
+    return <div className="score-main">Score: {this.props.count}</div>;
   }
 }
